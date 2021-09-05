1a8a865b3f79bf8f4dccf99ab5b00e131642375b
Sujai Ram Prasath C <20027296+sujairamprasathc@users.noreply.github.com>
2021-09-05 16:46:40 +0530

    Create FileConstants.h and FileConstants.cpp
    Project was missing these files. Failed to upload the two files during
    first commit.

---
+++ Indexer/FileConstants.h
@@ -0,0 +1,10 @@
+#ifndef FILE_CONSTANTS_H
+#define FILE_CONSTANTS_H
+
+#include <string>
+
+extern std::string INVERTED_INDEX_STORAGE_DIRECTORY;
+extern std::string FORWARD_INDEX_STORAGE_DIRECTORY;
+extern std::string LOCAL_APP_DATA_DIRECTORY;
+
+#endif /* FILE_CONSTANTS_H */
